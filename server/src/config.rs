d2a0121342bfaab55cd727e6ce20b459689ef8d8
Yibo Cao <cyb@ieee.org>
2021-07-24 13:35:19 -0700

    config

--- server/src/config.rs
+++ server/src/config.rs
@@ -7,19 +7,270 @@ use std::{cell::RefCell, rc::Rc, time::Duration};
 pub fn build_factory() -> Rc<RefCell<Factory>> {
     FactoryConfig {
         detail_cache: DetailCache::new(),
-        server: Server::new(1847),
+        server: Server::new(1848),
         min_cycle_time: Duration::from_secs(1),
         log_clients: vec!["1a"],
-        bus_accesses: vec![BasicAccess { client: "1a", addr: "enderstorage:ender_chest_1" }],
+        bus_accesses: vec![BasicAccess { client: "1a", addr: "cyclic:crate_4" }],
         backups: vec![],
     }
     .build(|factory| {
         factory.add_storage(ChestConfig {
             accesses: vec![BusAccess {
                 client: "1a",
-                inv_addr: "ironchest:obsidian_chest_1",
-                bus_addr: "enderstorage:ender_chest_1",
+                inv_addr: "colossalchests:interface_0",
+                bus_addr: "cyclic:crate_4",
             }],
-        })
+        });
+        factory.add_process(BufferedConfig {
+            name: "generator",
+            accesses: vec![BusAccess { client: "1a", inv_addr: "minecraft:dropper_6", bus_addr: "cyclic:crate_4" }],
+            slot_filter: None,
+            to_extract: None,
+            max_recipe_inputs: 0,
+            recipes: vec![],
+            stocks: vec![BufferedInput::new(Filter::Label("Charcoal"), 64)],
+        });
+        factory.add_process(BufferedConfig {
+            name: "plantSower",
+            accesses: vec![BusAccess {
+                client: "1a",
+                inv_addr: "modularrouters:item_router_2",
+                bus_addr: "cyclic:crate_4",
+            }],
+            slot_filter: None,
+            to_extract: None,
+            max_recipe_inputs: 0,
+            recipes: vec![BufferedRecipe {
+                outputs: vec![Output { item: Filter::Label("Redwood Log"), n_wanted: 1024 }],
+                inputs: vec![BufferedInput::new(Filter::Label("Redwood Sapling"), 1)],
+                max_inputs: i32::MAX,
+            }],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "outputA",
+            accesses: vec![BusAccess { client: "1a", inv_addr: "minecraft:dropper_10", bus_addr: "cyclic:crate_4" }],
+            slot_filter: None,
+            to_extract: extract_all(),
+            max_recipe_inputs: 0,
+            recipes: vec![],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "outputB",
+            accesses: vec![BusAccess { client: "1a", inv_addr: "minecraft:dropper_11", bus_addr: "cyclic:crate_4" }],
+            slot_filter: None,
+            to_extract: extract_all(),
+            max_recipe_inputs: 0,
+            recipes: vec![],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "outputC",
+            accesses: vec![BusAccess { client: "1a", inv_addr: "minecraft:dropper_13", bus_addr: "cyclic:crate_4" }],
+            slot_filter: None,
+            to_extract: extract_all(),
+            max_recipe_inputs: 0,
+            recipes: vec![],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "furnace",
+            accesses: vec![BusAccess {
+                client: "1a",
+                inv_addr: "modularrouters:item_router_0",
+                bus_addr: "cyclic:crate_4",
+            }],
+            slot_filter: None,
+            to_extract: None,
+            max_recipe_inputs: 8,
+            recipes: vec![
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Tin Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Zinc Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Lead Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Iron Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Gold Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Copper Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Osmium Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Silver Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Nickel Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Bismuth Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Uranium Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Aluminum Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Platinum Dust"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![Output { item: Filter::Label("Charcoal"), n_wanted: 64 }],
+                    inputs: vec![BufferedInput::new(Filter::Label("Redwood Log"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![Output { item: Filter::Label("Stone"), n_wanted: 64 }],
+                    inputs: vec![BufferedInput::new(Filter::Label("Cobblestone"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![Output { item: Filter::Label("Refined Iron Ingot"), n_wanted: 64 }],
+                    inputs: vec![BufferedInput::new(Filter::Label("Iron Ingot"), 1)],
+                    max_inputs: i32::MAX,
+                },
+            ],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "crusher",
+            accesses: vec![BusAccess {
+                client: "1a",
+                inv_addr: "modularrouters:item_router_1",
+                bus_addr: "cyclic:crate_4",
+            }],
+            slot_filter: None,
+            to_extract: None,
+            max_recipe_inputs: 8,
+            recipes: vec![
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Tin Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Zinc Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Gold Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Uranium Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Iron Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Osmium Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Platinum Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Copper Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Aluminum Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Yellorite Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Silver Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Bismuth Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Nickel Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![],
+                    inputs: vec![BufferedInput::new(Filter::Label("Lead Ore"), 1)],
+                    max_inputs: i32::MAX,
+                },
+            ],
+            stocks: vec![],
+        });
+        factory.add_process(BufferedConfig {
+            name: "platePress",
+            accesses: vec![BusAccess { client: "1a", inv_addr: "minecraft:dropper_12", bus_addr: "cyclic:crate_4" }],
+            slot_filter: None,
+            to_extract: None,
+            max_recipe_inputs: 8,
+            recipes: vec![
+                BufferedRecipe {
+                    outputs: vec![Output { item: Filter::Label("Iron Plate"), n_wanted: 16 }],
+                    inputs: vec![BufferedInput::new(Filter::Label("Iron Ingot"), 1)],
+                    max_inputs: i32::MAX,
+                },
+                BufferedRecipe {
+                    outputs: vec![Output { item: Filter::Label("Copper Plate"), n_wanted: 16 }],
+                    inputs: vec![BufferedInput::new(Filter::Label("Copper Ingot"), 1)],
+                    max_inputs: i32::MAX,
+                },
+            ],
+            stocks: vec![],
+        });
     })
 }
